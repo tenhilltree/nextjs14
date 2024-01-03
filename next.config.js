08122837f3433536e36048b9ba071c3656d1f7fe
tenhill <kpublic@163.com>
2024-01-03 17:02:18 +0800

    配置修改

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // output: "export",
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
