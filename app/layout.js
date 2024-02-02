35e424d56f290af7e83d5bdb6927d3a7b9199a5c
tenhill <kpublic@163.com>
2024-02-02 10:27:08 +0800

    react-spring

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import StyledComponentsRegistry from "@/lib/AntdRegistry";
+import StyledComponentsRegistry from "@/lib/AntdRegistry.jsx";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
--- lib/AntdRegistry.ts
+++ lib/AntdRegistry.ts
@@ -20,26 +20,27 @@ const theme = {
 const StyledComponentsRegistry = ({ children }) => {
   const cache = React.useMemo(() => createCache(), []);
   const isServerInserted = React.useRef(false);
-  useServerInsertedHTML(() => {
-    // 避免 css 重复插入
-    if (isServerInserted.current) {
-      return;
-    }
-    isServerInserted.current = true;
-    return (
-      <style
-        id="antd"
-        dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
-      />
-    );
-  });
-  return (
-    <StyleProvider cache={cache}>
-      <ConfigProvider locale={zhCN} theme={theme}>
-        {children}
-      </ConfigProvider>
-    </StyleProvider>
-  );
+  // useServerInsertedHTML(() => {
+  //   // 避免 css 重复插入
+  //   if (isServerInserted.current) {
+  //     return;
+  //   }
+  //   isServerInserted.current = true;
+  //   return (
+  //     <style
+  //       id="antd"
+  //       dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }}
+  //     />
+  //   );
+  // });
+  // return (
+  //   <StyleProvider cache={cache}>
+  //     <ConfigProvider locale={zhCN} theme={theme}>
+  //       {children}
+  //     </ConfigProvider>
+  //   </StyleProvider>
+  // );
+  return
 };
 
 export default StyledComponentsRegistry;
