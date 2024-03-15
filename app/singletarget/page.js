19218d3600b51d8792d69726c11445ab7bde263d
tenhill <kpublic@163.com>
2024-03-15 17:10:07 +0800

    singletarget

--- app/singletarget/page.js
+++ app/singletarget/page.js
@@ -1,22 +1,43 @@
 "use client";
 import React from "react";
+import { useDrag, useDrop, DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import styles from "./page.module.scss";
 
 let items = ["Glass", "Banana", "Paper"];
 
+function DragItem({ name }) {
+  const [{ active }, drag] = useDrag(() => ({
+    type: "a",
+    item: { name },
+    collect: (monitor, props) => {
+      console.log(props);
+      return {
+        active: monitor.isDragging(),
+      };
+    },
+  }));
+
+  return (
+    <div
+      ref={drag}
+      className={styles.dragItem}
+      style={{ opacity: active ? 0.5 : 1 }}
+    >
+      {name}
+    </div>
+  );
+}
+
 export default function SingleTarget() {
   return (
-    <>
-      <div className={styles.dropArea}></div>
+    <DndProvider backend={HTML5Backend}>
+      <div className={styles.dropArea}>Drag a box here</div>
       <div className={styles.dragArea}>
         {items.map((item) => {
-          return (
-            <div key={item} className={styles.dragItem}>
-              {item}
-            </div>
-          );
+          return <DragItem key={item} name={item} />;
         })}
       </div>
-    </>
+    </DndProvider>
   );
 }
