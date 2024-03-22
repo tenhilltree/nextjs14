652dfd577ce46a697921a49f269a8c5627fd9646
tenhill <kpublic@163.com>
2024-03-22 17:15:09 +0800

    emotion

--- app/antd/page.js
+++ app/antd/page.js
@@ -1,5 +1,38 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+
 import React from "react";
+import { css } from "@emotion/react";
 
 import styles from "./page.module.scss";
 
-export default function AntdPage() {}
+const style = css`
+  color: hotpink;
+`;
+
+const anotherStyle = css({
+  textDecoration: "underline",
+  "&:hover": {
+    color: "lightgreen",
+  },
+});
+
+export default function AntdPage() {
+  return (
+    <div css={style}>
+      Some hotpink text.
+      <div css={anotherStyle}>Some text with an underline.</div>
+      <div
+        css={{
+          textDecoration: "overline",
+          "&:hover": {
+            color: "red",
+          },
+          marginTop: 20,
+        }}
+      >
+        Some text with an underline.
+      </div>
+    </div>
+  );
+}
